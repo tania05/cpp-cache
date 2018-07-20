cdd0ebd9cf666cecd885dfb5a9b2b5192dca40c1
Tania Akter <tania9309@gmail.com>
2018-07-20 00:09:18 -0700

    Adding function for getting N1
    N1 being calculated from the original size being given, We get the nearest round number of the square root that's power of 2. Initially starts with the closest number nearest to square root and then increments or decrements till the new value divides the original n without remainder

--- include/ra/fft.hpp
+++ include/ra/fft.hpp
@@ -1,6 +1,39 @@
 #include <cstdio>
 #include <iostream>
 
+
+std::size_t get_n1(std::size_t n)
+{
+    //we round up if the next digit is 5 or more, otherwise round down.
+    //find the closest number to the square root
+    std::size_t n1 = (std::size_t)(std::sqrt(float(n)) + 0.5 );
+    std::size_t m1, m2;
+    m1 = m2 = n1;
+    
+    // std::cout << "n1: " << n1 << std::endl;
+            
+    
+    //find upper and lower number close to square root
+    while((n%m1 !=0) && (n%m2 !=0))
+    {
+        if(n%m1 !=0)
+        {
+            // cout << "incrementing m1 " << n%m1 << " " << m1 << endl;
+            ++m1;
+        }
+            
+        if(n%m2 !=0)
+        {
+        //   cout << "decrementing m2 " << n%m2 << " " << m2 << endl;
+             
+            --m2;
+        }
+    }
+    
+    return (n%m1) == 0  ? m1 : m2;
+}
+
+
 namespace ra::cache
 {
   template <class T>
