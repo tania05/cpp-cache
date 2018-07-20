c4d723e958002777d4ec4f59c3271ae7cb82c105
Tania Akter <tania9309@gmail.com>
2018-07-20 15:27:16 -0700

    Adding comments for matrix transpose

--- include/ra/matrix_transpose.hpp
+++ include/ra/matrix_transpose.hpp
@@ -23,20 +23,25 @@ namespace ra::cache{
           base_transpose(matrix,matrixb,startX,startY,x,y,ox,oy);
           return;
       }
-
+    
+    //Row is the biggest
       if ((x-startX) >= (y-startY)) {
           std::size_t xmid = (x-startX) / 2 + (x-startX)%2 ;
+          //split first half
           cotrans(matrix,matrixb,startX, startY, startX+xmid, y, ox, oy);
+          //split second half
           cotrans(matrix,matrixb,startX+xmid, startY, x, y, ox, oy );
           return;
       }
+      //column is the biggest
       else
       {
           std::size_t ymid = (y - startY)/ 2 + (y-startY)%2;
+          //split the first half
           cotrans(matrix,matrixb,startX, startY, x, startY+ymid,ox, oy);
+          //split the second half
           cotrans(matrix,matrixb,startX, startY+ymid, x, y,ox, oy );    
-      }
-      
+      }   
   }
 
   template<class T>
